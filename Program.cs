4bf333a7206c47b680c21dc4ca9dc1531f48cd78
Fabián Garrido <fgarridom@est.ups.edu.ec>
2020-11-20 05:47:33 -0500

    añadí la clase program y una clase de ejemplo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramaPruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
