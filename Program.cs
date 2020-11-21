30715c492faf54ae22832b49a4f4cd33175b2998
Fabián Garrido <fgarridom@est.ups.edu.ec>
2020-11-20 20:01:47 -0500

    nueva línea de código

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProgramaPruebaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("PROGRAMAR");
         }
     }
 }
